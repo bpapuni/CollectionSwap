160e578eb7db624ea22fdb01b2d20885b8b12d5a
bpapuni <brent.papuni@gmail.com>
2023-07-26 13:43:45 +1200

    Added database

---
+++ CollectionSwapDb/dbo/Tables/Collections.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Collections] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [Name]         NVARCHAR (MAX) NOT NULL,
+    [ItemListJSON] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_dbo.CardSets] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
