173f203003115623e5f450004b48baa00dc58f5b
bpapuni <brent.papuni@gmail.com>
2023-08-10 16:31:06 +1200

    Delete CollectionSwapDb directory

--- CollectionSwapDb/dbo/Tables/Collections.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Collections] (
-    [Id]           INT            IDENTITY (1, 1) NOT NULL,
-    [Name]         NVARCHAR (MAX) NOT NULL,
-    [ItemListJSON] NVARCHAR (MAX) NULL,
-    CONSTRAINT [PK_dbo.CardSets] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
