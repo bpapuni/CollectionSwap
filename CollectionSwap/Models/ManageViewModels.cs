6466560e4240b986ae110aa93ec94473c66c708e
bpapuni <brent.papuni@gmail.com>
2023-08-17 13:19:30 +1200

    regex condition added to change password, item quantity reduced

--- CollectionSwap/Models/ManageViewModels.cs
+++ CollectionSwap/Models/ManageViewModels.cs
@@ -90,11 +90,13 @@ namespace CollectionSwap.Models
         [Required(ErrorMessage = "This field is required.")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
+        [RegularExpression(@"^[^\s]*$", ErrorMessage = "Invalid Input (no spaces allowed)")]
         [Display(Name = "New Password")]
         public string NewPassword { get; set; }
 
         [Required(ErrorMessage = "This field is required.")]
         [DataType(DataType.Password)]
+        [RegularExpression(@"^[^\s]*$", ErrorMessage = "Invalid Input (no spaces allowed)")]
         [Display(Name = "Confirm New Password")]
         [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
